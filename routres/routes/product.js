bd964800b2794ee24b00e68c50236932780105bf
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-12-30 00:47:56 +0300

    create

--- routres/routes/product.js
+++ routres/routes/product.js
@@ -5,6 +5,7 @@ const authorization = require("./../middleWares/authorization");
 const {
   createProduct,
   getAllProduct,
+  getOneProduct,
   deleteProduct,
   search,
 } = require("./../controllers/product");
@@ -12,6 +13,8 @@ const {
 const productRouter = express.Router();
 productRouter.post("/create", authentication, authorization, createProduct); // اشتغل
 productRouter.get("/", authentication, getAllProduct); // اشتغل
+postsRouter.get("/oneProduct/:_id", authentication, getOneProduct); 
+
 productRouter.get("/search", search); 
 productRouter.delete("/delete/:_id", authentication, authorization, deleteProduct); // اشتغل
 
